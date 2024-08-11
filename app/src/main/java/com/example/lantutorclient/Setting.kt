4068f371bd53b5a6126e7c62e1fe085f2af8fa0d
Woody Moon <woodkill@gmail.com>
2024-08-11 12:50:58 +0900

    - 앱이 로딩되면 firestore에서 가장 최근에 등록된 사용자정보를 가져와서 Setting 화면을 띄우면서 보여준다. - 설정화면에서 새로시작을 누르면 입력된 정보를 firestore에 저장한다. - firestore function과 통신되는 거 확인

--- app/src/main/java/com/example/lantutorclient/Setting.kt
+++ app/src/main/java/com/example/lantutorclient/Setting.kt
@@ -1,6 +1,7 @@
 package com.example.lantutorclient
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +12,9 @@ import com.example.lantutorclient.databinding.FragmentSettingBinding
 import com.example.lantutorclient.*
 import com.example.lantutorclient.viewmodel.UserViewModel
 import com.google.firebase.firestore.FirebaseFirestore
-
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.ktx.functions
+import com.google.firebase.ktx.Firebase
 
 /**
  * A simple [Fragment] subclass.
@@ -22,6 +25,7 @@ class Setting : Fragment() {
 
     private lateinit var binding: FragmentSettingBinding
     private lateinit var db: FirebaseFirestore
+    private lateinit var functions: FirebaseFunctions
 
     // ViewModel 초기화 (Activity와 공유)
     val userViewModel: UserViewModel by activityViewModels()
@@ -41,6 +45,10 @@ class Setting : Fragment() {
         // Firestore 인스턴스 초기화
         db = FirebaseFirestore.getInstance()
 
+        // Firebase Functions 초기화
+        functions = FirebaseFunctions.getInstance()
+//        functions.useEmulator("127.0.0.1", 5001)
+
         userViewModel.userData.observe(viewLifecycleOwner, Observer { userData ->
             if (userViewModel.isUserInitiatedUpdate) {
                 // 사용자가 데이터를 직접 저장한 경우, UI 갱신을 생략
@@ -57,8 +65,9 @@ class Setting : Fragment() {
             }
         })
 
-        // 저장 버튼 클릭 리스너 설정
-        binding.saveSettingButton.setOnClickListener {
+        // 새로시작 버튼 클릭 리스너 설정
+        binding.startNewBtn.setOnClickListener {
+
             // UI에서 값을 가져와서
             val name = binding.etName.text.toString()
             val age = binding.etAge.text.toString()
@@ -66,6 +75,8 @@ class Setting : Fragment() {
             val learnLang = binding.etLearnLang.text.toString()
             val level = binding.etLevel.text.toString()
 
+            // 아래 내용들은 AI Assistant에게 알려줄 학생의 정보.
+            // 학생의 정보를 새로 알려주는 것은 새로 학습을 시작하겠다는 의미
             val user = mutableMapOf<String, Any>(
                 KEY_NAME to name,
                 KEY_AGE to age,
@@ -83,9 +94,12 @@ class Setting : Fragment() {
                 .add(user)
                 .addOnSuccessListener { docRef ->
                     println("DocumentSnapshot successfully added with ID: $docRef.id")
+                    // firestore에 새로 시작한 학생의 정보를 새로 만드는 것 성공.
                     // ViewModel 업데이트
                     user[KEY_DOC_ID] = docRef.id
                     userViewModel.updateUserData(user)
+                    // 새로운 학습 시작을 위한 cloud function을 호출.
+                    callCloudFunctionStartNew()
                 }
                 .addOnFailureListener { e ->
                     println("Error updating document: $e")
@@ -97,6 +111,45 @@ class Setting : Fragment() {
 
         // Inflate the layout for this fragment
         // return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    private fun callCloudFunctionStartNew() {
+
+//        functions
+//            .getHttpsCallable("on_request_example")
+//            .call()
+//            .addOnSuccessListener { result ->
+//                val response = result.data.toString()
+//                Log.d("Setting", response)
+//            }
+//            .addOnFailureListener { e ->
+//                Log.e("Setting", "Cloud Function failed", e)
+//            }
+//
+//        return
+
+        // userViewModel.userData 를 그냥 보낸다.
+        val data = userViewModel.userData.value
+        // Cloud Function 호출
+        data?.let {
+            // createdAt 은 제외
+            val mutableData = it.toMutableMap()
+            mutableData.remove(KEY_CREATED_AT)
+            functions
+                .getHttpsCallable("on_request_start_new")
+                .call(mutableData)
+                .addOnSuccessListener { result ->
+                    // Cloud Function에서 반환된 응답 처리
+                    val response = result.data as Map<*, *>
+                    val message = response["message"] as String
+                    Log.d("Setting", "Response from Cloud Function: $message")
+                }
+                .addOnFailureListener { e ->
+                    Log.e("Setting", "Cloud Function failed", e)
+                }
+        } ?: run {
+            Log.e("Setting", "No data available in userViewModel.userData")
+        }
     }
 
     companion object {
